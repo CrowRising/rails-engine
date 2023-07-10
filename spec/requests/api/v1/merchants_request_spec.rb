05dcd53a5f9c94b52122f33125deae536c6850e6
Crow Rising <miscbyrd@gmail.com>
2023-07-10 16:44:14 -0600

    test: complete test for first 2 endpoints

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,6 +8,29 @@ RSpec.describe 'Merchants API' do
       get '/api/v1/merchants'
 
       expect(response).to be_successful
+
+      merchants_info = JSON.parse(response.body, symbolize_names: true)
+      expect(merchants_info[:data].count).to eq(9)
+
+      merchants_info[:data].each do |merchant|
+        expect(merchant[:attributes]).to have_key(:name)
+        expect(merchant[:attributes][:name]).to be_a(String)
+      end
+    end
+
+    it 'can get one merchant by its id' do
+      id = create(:merchant).id
+
+      get "/api/v1/merchants/#{id}"
+
+      merchant_info = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+
+      expect(merchant_info[:data][:attributes]).to have_key(:name)
+      expect(merchant_info[:data][:attributes][:name]).to be_a(String)
+      expect(merchant_info[:data][:id]).to eq(id.to_s)
+
     end
   end
 end
