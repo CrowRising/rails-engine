30a826f3bce3cf5842b4fc2d5c550ba93a8c68c1
Crow Rising <miscbyrd@gmail.com>
2023-07-10 17:38:52 -0600

    fix: missing gitignore for coverage and missing tests

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -14,6 +14,17 @@ RSpec.describe 'Items API' do
       items = item_data[:data]
 
       expect(response).to be_successful
+
+      items.each do |item|
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes][:name]).to be_a(String)
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).to be_a(String)
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).to be_a(Float)
+        expect(item[:attributes]).to have_key(:merchant_id)
+        expect(item[:attributes][:merchant_id]).to be_a(Integer)
+      end
     end
   end
 end
