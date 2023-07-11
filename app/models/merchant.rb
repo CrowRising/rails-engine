6f6134875335d3608c0e0175513c80b731fd7b58
Crow Rising <miscbyrd@gmail.com>
2023-07-10 20:06:52 -0600

    feat: build AR query for merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,8 @@ class Merchant < ApplicationRecord
   has_many :items
 
   validates_presence_of :name
+
+  def self.find_by_name_fragment(fragment)
+    Merchant.where('name ILIKE ?', "%#{fragment}%").order(:name)
+  end
 end
