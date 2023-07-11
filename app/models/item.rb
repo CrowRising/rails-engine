a8bdee12f803f6c4cd4326d85e8d87b628d0b0a0
Crow Rising <miscbyrd@gmail.com>
2023-07-11 13:52:16 -0600

    refactor: method for find_by price

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,11 +8,12 @@ class Item < ApplicationRecord
                         :description,
                         :merchant_id
 
+
   def self.find_all(name: nil, min_price: nil, max_price: nil)
     if name != '' && name.present? && min_price.nil? && max_price.nil?
       Item.find_by_name_fragment(name)
     elsif (min_price.to_f.positive? || max_price.to_f.positive?) && name.nil?
-      Item.find_by_price(min_price:, max_price:)
+      Item.find_by_price(min_price: min_price, max_price: max_price)
     else
       false
     end
@@ -23,8 +24,8 @@ class Item < ApplicationRecord
   end
 
   def self.find_by_price(min_price: nil, max_price: nil)
-    @min_price = min_price.present? ? min_price : 0
-    @max_price = max_price.present? ? max_price : 0
+    min_price.present? ? @min_price  = min_price : @min_price = 0
+    max_price.present? ? @max_price = max_price : @max_price = 999999
     Item.where("unit_price BETWEEN #{@min_price} AND #{@max_price}")
   end
 end
