c9195b191cb4bc5ba3ec7ebd8561c62d414c7b98
Crow Rising <miscbyrd@gmail.com>
2023-07-10 19:39:43 -0600

    feat: add index action for items merchant controller

--- app/controllers/api/v1/items/merchant_controller.rb
+++ app/controllers/api/v1/items/merchant_controller.rb
@@ -4,6 +4,10 @@ module Api
   module V1
     module Items
       class MerchantController < ApplicationController
+
+        def index
+          render json: MerchantSerializer.new(Item.find(params[:item_id]).merchant)
+        end
       end
     end
   end
