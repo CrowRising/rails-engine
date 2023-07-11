f5f6813cbb68c520331da804069a462c82c077ed
Crow Rising <miscbyrd@gmail.com>
2023-07-10 22:41:27 -0600

    chore: add search oteam to controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/merchants/find', to: 'merchants/search#search'
+      get '/items/find_all', to: 'items/search#search'
 
       resources :merchants, only: %i[index show] do
         resources :items, only: [:index], controller: 'merchants/items'
