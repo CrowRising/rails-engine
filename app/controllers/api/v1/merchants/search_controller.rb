e6ebf086cb046dca4790765c2fddabbc0093a63b
Crow Rising <miscbyrd@gmail.com>
2023-07-10 20:31:50 -0600

    fix: typo in elsif statement

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -8,7 +8,7 @@ module Api
         def search
           if !params[:name] || params[:name].empty?
             render json: { error: 'Bad Request' }, status: 404
-          elseif Merchant.find_by_name_fragment(params[:name]) == []
+          elsif Merchant.find_by_name_fragment(params[:name]) == []
             render json: { data: [] }, status: 200
           elsif Merchant.find_by_name_fragment(params[:name])
             render json: MerchantSerializer.new(Merchant.find_by_name_fragment(params[:name]).first)
