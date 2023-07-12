ee691decd86533f924cc70e06d32ec02b4092a77
Crow Rising <miscbyrd@gmail.com>
2023-07-12 10:04:13 -0600

    refactor: commented out facade and moved method to model

--- app/facades/merchant_items_facade.rb
+++ app/facades/merchant_items_facade.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-class MerchantItemsFacade
-  def self.find_by_all_merch_id(merch_id)
-    Merchant.find(merch_id).items
-  end
-end
+# class MerchantItemsFacade
+#   def self.find_by_all_merch_id(merch_id)
+#     Merchant.find(merch_id).items
+#   end
+# end
