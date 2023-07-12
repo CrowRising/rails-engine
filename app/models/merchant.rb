68e51d9f058f85a4b0b68736bb348a04e08f4a23
Crow Rising <miscbyrd@gmail.com>
2023-07-12 10:05:00 -0600

    refactor: add find by all merch method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,4 +8,8 @@ class Merchant < ApplicationRecord
   def self.find_by_name_fragment(fragment)
     Merchant.where('name ILIKE ?', "%#{fragment}%").order(:name)
   end
+
+  def self.find_by_all_merch_id(merch_id)
+    Merchant.find(merch_id).items
+  end
 end
